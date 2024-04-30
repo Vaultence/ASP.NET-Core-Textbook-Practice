63dd696c5866ef15c9e055314d0af91313653197
Vaultence <calvin@hendersfamily.com>
2024-04-30 17:07:58 -0600

    More complex minimal API able to return a JSON of a Person record. - Also adds logging to the demonstration application

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
+using Microsoft.AspNetCore.HttpLogging;
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpLogging(opts =>
+ opts.LoggingFields = HttpLoggingFields.RequestProperties);
+builder.Logging.AddFilter(
+ "Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+WebApplication app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+ app.UseHttpLogging();
+}
 app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/person", () => new Person("Andrew", "Lock"));
 app.Run();
+public record Person(string FirstName, string LastName);
