87b28a8164a74aa1771faf43a09bd1764ebb30a3
Vaultence <calvin@hendersfamily.com>
2024-05-08 14:13:46 -0600

    404 and 400 results replaced with equivalent problem details

--- FruitPostExample/Program.cs
+++ FruitPostExample/Program.cs
@@ -10,13 +10,15 @@ app.MapGet("/fruit", () => _fruit);
 app.MapGet("/fruit/{id}", (string id) =>
     _fruit.TryGetValue(id, out var fruit)
         ? TypedResults.Ok(fruit)
-        : Results.NotFound());
+        : Results.Problem(statusCode: 404));
 
 app.MapPost("/fruit/{id}", (string id, Fruit fruit) =>
     _fruit.TryAdd(id, fruit)
     ? TypedResults.Created($"/fruit/{id}", fruit)
-        : Results.BadRequest(new
-        { id = "A fruit with this id already exists" }));
+        : Results.ValidationProblem(new Dictionary<string, string[]>
+        {
+            {"id", new string[] {"A fruit with this id already exists"}}
+        }));
 
 app.MapPut("/fruit/{id}", (string id, Fruit fruit) =>
 {
